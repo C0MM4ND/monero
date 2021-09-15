bdd9cddb96483579226b207a70d7a906b79658ef
Command <maoxs2@gmail.com>
2021-09-16 00:27:05 +0800

    change network_id for stagenet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -260,8 +260,8 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 38081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 38082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
-      } }; // Bender's daydream
+        0x69 ,0x63, 0x61, 0x6E, 0x65, 0x61 , 0x74, 0x67, 0x6C, 0x61, 0x73, 0x73, 0x00, 0x00, 0x00, 0x00
+      } }; // Bender's daydream -> 69 63 61 6e 65 61 74 67 6c 61 73 73 icaneatglass
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
     uint32_t const GENESIS_NONCE = 10002;
   }
